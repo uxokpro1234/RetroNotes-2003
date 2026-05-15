d38c96499e9ef709933a6d626badb548bdd17136
uxokpro1234 <81376237+uxokpro1234@users.noreply.github.com>
2026-05-15 22:46:29 +0300

    Add files via upload

---
+++ src/main/java/me/uxokpro1234/retronotes/config/AppConfig.java
@@ -0,0 +1,32 @@
+package me.uxokpro1234.retronotes.config;
+
+import java.awt.*;
+
+public class AppConfig {
+
+    // APP INFO
+    public static final String APP_NAME = "RetroNotes 2003 Professional Edition";
+
+    public static final String VERSION = "Build 1.0.3";
+
+    // WINDOW
+    public static final int WINDOW_WIDTH = 950;
+    public static final int WINDOW_HEIGHT = 700;
+
+    // COLORS
+    public static final Color BACKGROUND = new Color(192,192,192);
+    public static final Color HEADER_BLUE = new Color(0,0,128);
+    public static final Color NOTE_LIST_BG = new Color(245,245,245);
+    public static final Color TEXT_AREA_BG = new Color(250,250,250);
+    public static final Color LOG_GREEN = new Color(0,255,0);
+
+    //FONTS
+    public static final Font UI_FONT = new Font("Tahoma", Font.PLAIN, 11);
+    public static final Font TITLE_FONT = new Font("Arial", Font.BOLD, 28);
+
+    public static final Font CONSOLE_FONT = new Font("Courier New", Font.PLAIN, 12);
+
+    // BUTTONS
+    public static final Dimension TOOLBAR_BUTTON_SIZE = new Dimension(90,25);
+
+}
